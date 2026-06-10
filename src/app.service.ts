1edf8c0d0611e16dbca45eca06ab0c69d43f38b4
muhammadsyahidulhaq <muhammad.syahidulhaq@widyatama.ac.id>
2026-06-10 23:20:05 +0700

    Menambahkan fitur perkalian dan test

--- src/app.service.ts
+++ src/app.service.ts
@@ -10,4 +10,12 @@ export class AppService {
   add(a: number, b: number): number {
     return a + b;
   }
+  multiply(a: number, b: number): number {
+  return a * b;
+}
+// tambahkan di app.service.spec.ts
+it('harus mengalikan dua angka dengan benar', () => {
+  expect(service.multiply(3, 4)).toBe(12);
+});
+
 }
