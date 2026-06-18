7a0391f0944d3ba0e64a3ce6bf2363ce55920f10
muhammadsyahidulhaq <muhammad.syahidulhaq@widyatama.ac.id>
2026-06-18 21:59:09 +0700

    Initial monorepo setup

---
+++ beckendsbo/src/app.service.spec.ts
@@ -0,0 +1,24 @@
+import { AppService } from './app.service';
+
+describe('AppService', () => {
+  let service: AppService;
+
+  beforeEach(() => {
+    service = new AppService();
+  });
+
+  it('harus mengembalikan "Hello World!"', () => {
+    expect(service.getHello()).toBe('Hello World!');
+  });
+
+  it('harus menjumlahkan dua angka dengan benar', () => {
+    expect(service.add(2, 3)).toBe(5);
+    expect(service.add(-1, 1)).toBe(0);
+  });
+  
+// tambahkan di app.service.spec.ts
+it('harus mengalikan dua angka dengan benar', () => {
+  expect(service.multiply(3, 4)).toBe(12);
+});
+
+});
---
+++ beckendsbo/src/app.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class AppService {
+  getHello(): string {
+    return 'Hello World!';
+  }
+
+  // Fungsi sederhana untuk diuji
+  add(a: number, b: number): number {
+    return a + b;
+  }
+  multiply(a: number, b: number): number {
+  return a * b;
+}
+}
