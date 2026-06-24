d6a9e2a2a64a33377b7ad8bf553f55dfcae0b659
muhammadsyahidulhaq <muhammad.syahidulhaq@widyatama.ac.id>
2026-06-24 22:03:50 +0700

    SCRUM-50 nambah fitur crud organisasi+

--- beckendsbo/src/organizations/organizations.service.spec.ts
+++ beckendsbo/src/organizations/organizations.service.spec.ts
@@ -1,12 +1,21 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { OrganizationsService } from './organizations.service';
+import { PrismaService } from '../prisma/prisma.service';
 
 describe('OrganizationsService', () => {
   let service: OrganizationsService;
 
+  const mockPrisma = {};
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [OrganizationsService],
+      providers: [
+        OrganizationsService,
+        {
+          provide: PrismaService,
+          useValue: mockPrisma,
+        },
+      ],
     }).compile();
 
     service = module.get<OrganizationsService>(OrganizationsService);
